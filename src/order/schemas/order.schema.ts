381903fef229d9de6a5e54fb13cd35254a27da16
Minh-Khoi <rocketme144@gmail.com>
2023-06-13 01:42:02 +0700

    chore : ref on user on order

--- src/order/schemas/order.schema.ts
+++ src/order/schemas/order.schema.ts
@@ -1,6 +1,7 @@
 import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import { modelOptions, prop } from '@typegoose/typegoose';
 import { Types } from 'mongoose';
+import { User } from 'src/user/schemas/user.schema';
 import { getEnumValues } from '../../utils/enum-utils';
 import { OrderStatus } from '../enums/order-status.enum';
 import { OrderProduct } from './order-product.schema';
@@ -34,7 +35,7 @@ export class Order {
    * Order user ID
    */
   @ApiProperty({ type: String })
-  @prop({ type: Types.ObjectId })
+  @prop({ type: Types.ObjectId, ref: () => User })
   user_id?: Types.ObjectId;
 
   @prop({ required: true })
